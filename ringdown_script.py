362ee05fcad87622928df681099939865b7164eb
Selma Vangstein <selmavangstein@gmail.com>
2025-11-30 09:42:49 +0000

    more start times in run

--- ringdown_script.py
+++ ringdown_script.py
@@ -4,17 +4,29 @@ import scrapbook as sb
 import tempfile
 import os
 
-num_modes = [1, 2]
-start_offsets = [4, 6, 8, 10.5, 12]
-
-#num_modes = [2]
-#start_offsets = [6, 10.5]
-
+# this block is if we want to run one mode for some start times and two modes for others
+#num_modes = [1, 2]
+#start_offsets2 = [5, 6, 7, 8, 9, 10, 10.5, 11]
+#start_offsets1 = [12, 13, 14, 15, 16, 17, 18, 19, 20]
 # Define your parameter combinations
+# parameter_sets = [
+#     {"num_modes": nm, "analysis_start_time": so, "output_file": f"fitresults/fit_result_modes_{nm}_start_{str(so).replace('.', '')}.json"} for nm in num_modes for so in start_offsets2
+# ] + [
+#     {"num_modes": 1, "analysis_start_time": so, "output_file": f"fitresults/fit_result_modes_1_start_{str(so).replace('.', '')}.json"} for so in start_offsets1
+# ]
+
+num_modes = [2]
+start_offsets = [12]
+
 parameter_sets = [
     {"num_modes": nm, "analysis_start_time": so, "output_file": f"fitresults/fit_result_modes_{nm}_start_{str(so).replace('.', '')}.json"} for nm in num_modes for so in start_offsets
 ]
 
+
+print(f"Total configurations to run: {len(parameter_sets)}")
+print("full parameter sets:")
+for params in parameter_sets:
+    print(params)
 # Path to your notebook
 notebook_path = "./bs_ringdown.ipynb"
 
